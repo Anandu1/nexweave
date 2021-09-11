e4c9811dce561dc5b16dae9adb1335bc25907c0e
anandu m <anandu1566@gmail.com>
2021-09-11 14:14:44 +0530

    Add existing file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@
 import './App.css';
 import Campaign from './Components/Campaign/Campaign';
 import Sidebar from './Components/Sidebar/Sidebar';
-
+import {Route,Switch} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
    <Sidebar  />
+   <Switch>
+   <Route exact path="/" component={Campaign}/>
+   <Route path="/campaign" component={Campaign}/>
+   <Route path="/get-started" />
+   <Route path="/templates" />
+   <Route path="/integrations" />
+   <Route path="/manage-settings" />
+   <Route path="/settings" />
    <Campaign />
+   </Switch>
     </div>
   );
 }
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -8,37 +8,98 @@ import SettingsIcon from '@material-ui/icons/Settings';
 import NightsStayIcon from '@material-ui/icons/NightsStay';
 import './Sidebar.css'
 import CardPro from '../Campaign/Card/Card';
+import { useHistory } from "react-router-dom";
+import {useState} from 'react'
 function Sidebar() {
+    const history = useHistory();
+    const [active, setActive] = useState("Campaign")
+    function handleClick() {
+        history.push("/get-started");
+        setActive("get-started")
+      }
+      function handleTemplates() {
+        history.push("/templates");
+        setActive("templates")
+      }
+      function handleCampaigns() {
+        history.push("/campaign");
+        setActive("Campaign")
+      }
+      function handleIntegrations() {
+        history.push("/integrations");
+        setActive("integrations")
+      }
+      function handleManageS() {
+        history.push("/manage-settings");
+        setActive("manage-settings")
+      }
+      function handleSettings() {
+        history.push("/settings");
+        setActive("settings")
+      }
     return (
         <div className="sidebar">
             <div className="sidebarWrapper">
                 <div className="sidebarMenu">
-                <li className="sidebarTitle">
+                <li 
+                 className="sidebarTitle">
                             <NightsStayIcon style={{color:"rgb(58, 12, 87)" ,marginRight:"15px" }}
                              className="sidebarIcon" />NexWeave
                         </li>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem">
+                        <div onClick={handleClick}>
+                        <li 
+                        style={{
+                            color:active=="get-started" ? "rgb(58, 12, 87)" : "gray"
+                        }}
+                        className="sidebarListItem">
                             <HourglassEmptyIcon style={{color:"rgb(58, 12, 87)" ,marginRight:"15px" }}
                              className="sidebarIcon" />Get Started
                         </li>
-                        <li className="sidebarListItem">
+                        </div>
+                      
+                        <li
+                          style={{
+                            color:active=="templates" ? "rgb(58, 12, 87)" : "gray"
+                        }}
+                        onClick={handleTemplates}
+                         className="sidebarListItem">
                             <GridOnIcon style={{color:"rgb(58, 12, 87)" ,marginRight:"15px" }}
                              className="sidebarIcon"/>Templates
                         </li>
-                        <li className="sidebarListItem">
+                        <li
+                          style={{
+                            color:active=="Campaign" ? "rgb(58, 12, 87)" : "gray"
+                        }}
+                        onClick={handleCampaigns}
+                        className="sidebarListItem">
                             <PeopleOutlineIcon style={{color:"rgb(58, 12, 87)" ,marginRight:"15px" }}
                              className="sidebarIcon"/>Campaigns
                         </li>
-                        <li className="sidebarListItem">
+                        <li
+                          style={{
+                            color:active=="integrations" ? "rgb(58, 12, 87)" : "gray"
+                        }}
+                        onClick={handleIntegrations}
+                         className="sidebarListItem">
                             <SettingsInputCompositeIcon style={{color:"rgb(58, 12, 87)" ,marginRight:"15px" }}
                              className="sidebarIcon" />Integrations
                         </li>
-                        <li className="sidebarListItem">
+                        <li
+                          style={{
+                            color:active=="manage-settings" ? "rgb(58, 12, 87)" : "gray"
+                        }}
+                        onClick={handleManageS}
+                         className="sidebarListItem">
                             <WebIcon style={{color:"rgb(58, 12, 87)" ,marginRight:"15px" }}
                              className="sidebarIcon"/>Manage Websites
                         </li>
-                        <li className="sidebarListItem">
+                        <li
+                         style={{
+                            color:active=="settings" ? "rgb(58, 12, 87)" : "gray"
+                        }}
+                        onClick={handleSettings}
+                         className="sidebarListItem">
                             <SettingsIcon style={{color:"rgb(58, 12, 87)" ,marginRight:"15px" }}
                             className="sidebarIcon"/>Settings
                         </li>
@@ -48,7 +109,7 @@ function Sidebar() {
                 </div>
             </div>
             <div className="card">
-                    <CardPro style={{marginTop:"60px",marginLeft:"15px",left:"-50px"}}/>
+                    <CardPro style={{marginTop:"60px",marginLeft:"15px"}}/>
                     </div>
         </div>
     )
